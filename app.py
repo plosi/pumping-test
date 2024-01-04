fe8b9e5f6ecd60e1db5d338641832cab090e06cd
pl0s1 <paolo.mizu@gmail.com>
2024-01-04 16:23:06 +0100

    first MVP

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import shinyswatch
 
 import pandas as pd
 
-from functions import import_data_from_file, calc_drawdown, normal_drawdown_plot
+from functions import import_data_from_file, step_test_plot, normal_drawdown_plot, preview_plot, cooper_jacob, calc_estimated_yield
 
 ### UI ###
 
@@ -37,18 +37,11 @@ main_tabs = ui.navset_card_tab(
         'Data',
         ui.row(
             ui.column(
-                6,
+                4,
                 ui.output_data_frame('show_df'),
             ),
-            # ui.panel_conditional(
-            #     'output.filein',
-            #     ui.column(
-            #         6,
-            #         ui.output_data_frame('show_df')
-            #     )
-            # ),
             ui.column(
-                6,
+                8,
                 ui.panel_conditional(
                     'output.show_df',
                     ui.output_plot('plot_preview')
@@ -57,28 +50,49 @@ main_tabs = ui.navset_card_tab(
         ),
     ),
     ui.nav_panel(
-        'Test',
+        'Analysis',
         ui.panel_conditional(
             'output.show_df',
             ui.row(
                 ui.column(
                     3,
-                    plot_select,
-                    ui.input_switch(
-                        id='reverse_y',
-                        label='Reverse y-axis',
-                        value=True,
+                    # ptest_select,
+                    # ui.input_switch(
+                    #     id='reverse_y',
+                    #     label='Reverse y-axis',
+                    #     value=True,
+                    # ),
+                    # ui.input_text(
+                    #     id='plot_title',
+                    #     label='Chart title',
+                    #     placeholder='Your title here'
+                    # ),
+                    ui.panel_conditional(
+                        'input.ptest === "constant" || input.ptest === "recovery"',
+                        ui.input_slider(
+                            id='guess',
+                            label='Linear fit',
+                            min=2,
+                            max=100,# max length of df
+                            value=15# half length of df
+                        ),
                     ),
-                    ui.input_text(
-                        id='plot_title',
-                        label='Chart title',
-                        placeholder='Your title here'
-                    )
+                    ui.panel_conditional(
+                        'input.ptest === "step_drawdown"',
+                        # ui.input_numeric(
+                        #     id='static_level',
+                        #     label='Static Level [mbd]',
+                        #     value=10.0,
+                        #     min=0,
+                        #     width='75%'
+                        # ),
+                    ),
+                    ui.output_text_verbatim('txt'),
                 ),
-                # ui.column(
-                #     9,
-                #     ui.output_plot('plot')
-                # ),
+                ui.column(
+                    9,
+                    ui.output_plot('plot')
+                ),
             ),
         ),
     )
@@ -89,7 +103,61 @@ app_ui = ui.page_fluid(
     ui.panel_title(title='HumPumpTest', window_title='Pumping Test App'),
     ui.layout_sidebar(
         ui.sidebar(
-            # ptest_select,
+            ptest_select,
+            ui.panel_conditional(
+                'input.ptest',
+                ui.input_numeric(
+                    id='static_level',
+                    label='Static Level [mbd]',
+                    value=10.0,
+                    min=0,
+                    # width='75%'
+                ),
+            ),
+            ui.panel_conditional(
+                'input.ptest === "step_drawdown"',
+                # ui.input_numeric(
+                #     id='static_level',
+                #     label='Static Level [mbd]',
+                #     value=10.0,
+                #     min=0,
+                #     width='75%'
+                # ),
+                ui.input_slider(
+                    id='no_steps',
+                    label='Number of Steps',
+                    min=2,
+                    max=6,
+                    value=4
+                ),
+                ui.output_ui('steps_flow_time'),
+            ),
+            ui.panel_conditional(
+                'input.ptest === "constant" || input.ptest === "recovery"',
+                ui.input_numeric(
+                    id='flowrate',
+                    label='Average Pumping Flowrate [m\u00b3/day]',
+                    value=24.0,
+                    # width='75%'
+                ),
+            ),
+            ui.panel_conditional(
+                'input.ptest === "recovery"',
+                # ui.input_numeric(
+                #     id='static_level',
+                #     label='Static Level [mbd]',
+                #     value=10.0,
+                #     min=0,
+                #     width='75%'
+                # ),
+                 ui.input_numeric(
+                    id='end_of_pumping',
+                    label='End of Pumping [min]',
+                    value=600.0,
+                    min=0,
+                    # width='75%'
+                ),
+            ),
             ui.input_radio_buttons(
                 id='data_source',
                 label='Load data',
@@ -103,10 +171,23 @@ app_ui = ui.page_fluid(
             ui.panel_conditional(
                 'input.data_source === "m"',
                 ui.p('Add your data directly in the table')
-            )
+            ),
+            ui.panel_conditional(
+                'output.show_df',
+                ui.input_switch(
+                    id='reverse_y',
+                    label='Reverse y-axis',
+                    value=True,
+                ),
+                ui.input_text(
+                    id='plot_title',
+                    label='Chart title',
+                    placeholder='Your title here'
+                ),
+            ),
         ),
-        ui.output_text_verbatim('txt'),
-        main_tabs
+        main_tabs,
+        # ui.output_text_verbatim('txt'),
     ),
 )
 
@@ -117,17 +198,91 @@ def server(input, output, session):
     @output
     @render.text
     def txt():
-        return f'Plot: {input.plot()}'
-    
+        # return f'Plot: {input.plot()}'
+        return get_test_results()
+
+    @reactive.Calc
+    def get_test_results():
+        res = 'results'
+        df = load_df_from_csv()
+        guess = input.guess()
+        Q = input.flowrate()
+        if input.ptest() == 'constant':
+            ds = normal_drawdown_plot(df.time_min, df.drawdown_m, guess=guess)[1]['ds']
+            T = cooper_jacob(Q, ds)
+            y = calc_estimated_yield(T)
+            m, c = normal_drawdown_plot(df.time_min, df.drawdown_m, guess=guess)[1]['y_fit_m'], normal_drawdown_plot(df.time_min, df.drawdown_m, guess=guess)[1]['y_fit_c']
+            res = f'ds = {m:.3f}\u00b7t + {c:.3f}\n\nQ: {Q:.2f} m\u00b3/day\nds: {ds:.2f} m\nT: {T:.2f} m\u00b2/day\nYield: {y:.2f} m\u00b3/day'
+        elif input.ptest() == 'recovery':
+            ds = normal_drawdown_plot(df['t/t\''], df.drawdown_m, guess=guess)[1]['ds']
+            T = cooper_jacob(Q, ds, recovery=True)
+            y = calc_estimated_yield(T)
+            m, c = normal_drawdown_plot(df['t/t\''], df.drawdown_m, guess=guess)[1]['y_fit_m'], normal_drawdown_plot(df['t/t\''], df.drawdown_m, guess=guess)[1]['y_fit_c']
+            res = f'ds = {m:.3f}\u00b7t + {c:.3f}\n\nQ: {Q:.2f} m\u00b3/day\nds: {ds:.2f} m\nT: {T:.2f} m\u00b2/day\nYield: {y:.2f} m\u00b3/day'
+        elif input.ptest() == 'step_drawdown':
+            dff = get_specific_drawdown()
+            s_Q =dff['s/Q']
+            tmp = step_test_plot(dff.Q, dff['s/Q'], reverse_y=input.reverse_y())[1]
+            B = tmp['B']
+            C = tmp['C']
+            BQ = tmp['BQ']
+            CQ2 = tmp['CQ2']
+            dff['eff'] = [bq/(bq+cq2) for bq,cq2 in zip(BQ,CQ2)]
+            res = f'y = {C:.3f}\u00b7Q + {B:.3f}\n\nEffiency: ' + str([round(e*100,2) for e in dff.eff])
+        return res
+
     @reactive.Calc
     def load_df_from_csv():
         fname: list[FileInfo] = input.filein()
         if not fname:
             return
         df = import_data_from_file(fname[0]['datapath'])
-        df['drawdown_m'] = calc_drawdown(df.level_m)
+        df['drawdown_m'] = df.level_m - input.static_level()#calc_drawdown(df.level_m)
+        if input.ptest() == 'recovery':
+            # df['residual_ds'] = df.level_m - input.static_level()
+            time = input.end_of_pumping() + df.time_min
+            df['t/t\''] = time / df.time_min
         return df
     
+    @reactive.Calc
+    def get_specific_drawdown():
+        df = load_df_from_csv()
+        time = [input[f'end_s{step}']() for step in range(1, input.no_steps()+1)]
+        avg_q = [input[f'flowrate_s{step}']() for step in range(1, input.no_steps()+1)]
+        ds = [3.2,8,14.7,19]
+        # for t in time:
+        #     ds.append(df[df.time_min == t][['drawdown_m']])
+        spec_ds = [i / j for i,j in zip(ds, avg_q)]
+        return pd.DataFrame(list(zip(time,ds,avg_q,spec_ds)), columns=['time','ds','Q','s/Q'])
+
+    @output
+    @render.ui
+    def steps_flow_time():
+        input_block = []
+        ucode = ['\u2081', '\u2082', '\u2083', '\u2084', '\u2085', '\u2086']
+        for step in range(1, input.no_steps()+1):
+            input_block.append(
+                ui.row(
+                    ui.column(
+                        6,
+                        ui.input_numeric(
+                            id=f'flowrate_s{step}',
+                            label=f'Q{ucode[step-1]} [m\u00b3/h]',
+                            value=5.0,
+                        ),
+                    ),
+                    ui.column(
+                        6,
+                        ui.input_numeric(
+                            id=f'end_s{step}',
+                            label=f't{ucode[step-1]} [min]',
+                            value=120
+                        ),
+                    )
+                )
+            )
+        return input_block
+
     @output
     @render.data_frame
     def show_df():
@@ -148,27 +303,28 @@ def server(input, output, session):
     def plot_preview():
         # df = pd.read_csv('_df.csv')
         df = load_df_from_csv()
-        title = input.plot_title()
-        return normal_drawdown_plot(df.time_min, df.level_m, reverse_y=input.reverse_y(), title=title)
-        # if input.plot() == 'normal_dd':
-        #     return normal_drawdown_plot(df.time_min, df.drawdown_m, reverse_y=input.reverse_y(), title=title)
-        # elif input.plot() == 'semilog_dd':
-        #     return normal_drawdown_plot(df.time_min, df.drawdown_m, reverse_y=input.reverse_y(), title=title, log_x=True)
-        # elif input.plot() == 'normal_wl':
-        #     return normal_drawdown_plot(df.time_min, df.level_m, reverse_y=input.reverse_y(), title=title)
+        title = 'Water Level Vs. Time'#input.plot_title()
+        vlines = []
+        if input.ptest() == 'step_drawdown':
+            for step in range(1, input.no_steps()+1):
+                vlines.append(input[f'end_s{step}']())
+        return preview_plot(df.time_min, df.level_m, reverse_y=input.reverse_y(), title=title, vlines=vlines)
     
     @output
-    @render.plot()
+    @render.plot(width=800, height=800)
     def plot():
         # df = pd.read_csv('_df.csv')
         df = load_df_from_csv()
         title = input.plot_title()
-        if input.plot() == 'normal_dd':
-            return normal_drawdown_plot(df.time_min, df.drawdown_m, reverse_y=input.reverse_y(), title=title)
-        elif input.plot() == 'semilog_dd':
-            return normal_drawdown_plot(df.time_min, df.drawdown_m, reverse_y=input.reverse_y(), title=title, log_x=True)
-        elif input.plot() == 'normal_wl':
-            return normal_drawdown_plot(df.time_min, df.level_m, reverse_y=input.reverse_y(), title=title)
+        guess = input.guess()
+        if input.ptest() == 'step_drawdown':
+            dff = get_specific_drawdown()
+            return step_test_plot(dff.Q, dff['s/Q'], reverse_y=input.reverse_y(), title=title)[0]
+        elif input.ptest() == 'constant':
+            return normal_drawdown_plot(df.time_min, df.drawdown_m, reverse_y=input.reverse_y(), title=title, log_x=True, guess=guess)[0]
+        elif input.ptest() == 'recovery':
+            # df['residual_ds'] = df.level_m - input.static_level()
+            return normal_drawdown_plot(df['t/t\''], df.drawdown_m, reverse_y=input.reverse_y(), title=title, log_x=True, guess=guess)[0]
 
     # @output
     # @render.text
--- functions.py
+++ functions.py
@@ -28,6 +28,141 @@ def linear(x, a, b):
     """
     return a * x + b
 
+def cooper_jacob(Q:float, ds:float, recovery=False):
+    """
+    Q: average pumping flowrate [m^3/day]
+    ds: drawdown from semi-log chart [m]
+    
+    Return the aquifer's transmissivity in m^2/day according to Cooper-Jacob's
+    formula (semplification of Theis' method):
+    T = 0.180*Q/ds
+    """
+    if not recovery:
+        return 0.180 * Q / ds
+    else:
+        return 0.183 * Q / ds
+
+def calc_estimated_yield(T:float):
+    """
+    T: transmissivity [m^2/day]
+    
+    Return the estimated yield of the aquifer in m^3/day, based on MacDonald et al (2005):
+    for a borehole supplying 5,000 litres per day, the transmissivity value of the aquifer 
+    should be at least 1 m^2/day. An aquifer with a transmissivity of 10 m^2/day would be
+    capable of yielding around 40,000 litres per day.
+    """
+    return 40 * T / 10
+
+def step_test_plot(Q:pd.Series, spec_ds:pd.Series, title='Your title here'):
+    """
+    Q: yield, as input by user
+    spec_ds: specific drawdown, calculated as s/Q
+    """
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(20,15))
+    fig.tight_layout()
+
+    Q = Q.astype('float')
+    popq, _ = curve_fit(linear, Q, spec_ds)
+    c, b = popq[0], popq[1]
+    
+    y_fit = c * Q + b
+
+    BQ = [b * q for q in Q]
+    CQ2 = [c * q**2 for q in Q]
+    BQ_CQ2 = [bq+cq2 for bq,cq2 in zip(BQ,CQ2)]
+
+    sns.scatterplot(
+        ax=ax[0],
+        x=Q,
+        y=spec_ds
+    )
+
+    sns.lineplot(
+        ax=ax[0],
+        x=Q,
+        y=y_fit,
+        color='red',
+        linewidth=1,
+    )
+
+    sns.lineplot(
+        ax=ax[1],
+        x=Q,
+        y=BQ_CQ2,
+        color='blue',
+        linewidth=1,
+        label='BQ + CQ\u00b2'
+    )
+
+    sns.lineplot(
+        ax=ax[1],
+        x=Q,
+        y=BQ,
+        color='orange',
+        linewidth=1,
+        label='BQ'
+    )
+    
+    ax[0].set_xlabel('Yield [m\u00b3/h]', size=14)
+    ax[0].set_ylabel('Specific Drawdown [m\u00b2/h]', size=14)
+    ax[0].set_title(f'{title} - Specific Drawdown Vs. Yield', size=22)
+
+    ax[1].set_xlabel('Yield [m\u00b3/h]', size=14)
+    ax[1].set_ylabel('Head losses [m]', size=14)
+    ax[1].set_title(f'{title} - Linear Vs. Non-linear Head Losses', size=22)
+    ax[1].legend(loc='best')
+
+    # text = f'{m:.3f}\u00b7t + {c:.3f}'
+    # plt.text(x=1.2, y=.2, s=text, horizontalalignment='left', size='large')
+
+    ax[0].grid()
+    ax[1].grid()
+
+    d = {
+        'B': b,
+        'C': c,
+        'BQ': BQ,
+        'CQ2': CQ2
+        }
+
+    return fig, d
+
+
+def preview_plot(t:pd.Series, ds:pd.Series, reverse_y=True, title='Your title here', vlines=[]):
+    """
+    t: time elapsed
+    ds: drawdown in m
+    """
+    plt.figure(figsize=(20,15))
+
+    ax = sns.scatterplot(
+        x=t,
+        y=ds
+    )
+
+    if reverse_y:
+        plt.gca().invert_yaxis()
+        plt.xlim(0)
+        plt.ylim(top=0)
+    else:
+        plt.xlim(0)
+        plt.ylim(0)
+    
+    for x in vlines:
+        plt.axvline(x=x, color='red', linestyle='--')
+    
+    plt.xlabel('Elapsed time [min]', size=14)
+    plt.ylabel('Water level [m]', size=14)
+    plt.title(f'{title}', size=22)
+    
+    plt.tight_layout()
+    plt.grid()
+    
+    # plt.savefig(f'{bh_name}_Constant.png')
+    # plt.show()
+    return ax
+
+
 def normal_drawdown_plot(t:pd.Series, ds:pd.Series, reverse_y=True, title='Your title here', log_x=False, guess=15, t0=10):
     """
     t: time elapsed
@@ -35,23 +170,24 @@ def normal_drawdown_plot(t:pd.Series, ds:pd.Series, reverse_y=True, title='Your 
     """
     plt.figure(figsize=(20,15))
     
-    # t_crop, ds_crop = t[guess:], ds[guess:]
-    # popt, pcov = curve_fit(linear, t_crop, ds_crop)
-    # m, c = popt[0], popt[1]
+    t = t.astype('float')
+    t_crop, ds_crop = np.log(t[1:guess+1]), ds[1:guess+1]
+    popt, _ = curve_fit(linear, t_crop, ds_crop)
+    m, c = popt[0], popt[1]
     
-    # y_fit = m * t + c
-    
+    y_fit = m * np.log(t) + c
+
     ax = sns.scatterplot(
         x=t,
         y=ds
     )
     
-    # sns.lineplot(
-    #     x=t,
-    #     y=y_fit,
-    #     color='red',
-    #     linewidth=1
-    # )
+    sns.lineplot(
+        x=t,
+        y=y_fit,
+        color='red',
+        linewidth=1
+    )
 
     if reverse_y:
         plt.gca().invert_yaxis()
@@ -65,19 +201,40 @@ def normal_drawdown_plot(t:pd.Series, ds:pd.Series, reverse_y=True, title='Your 
         plt.xscale('log')
         plt.minorticks_on()
         plt.xlim(0)
-        # ylim = ax.get_ylim()[1]
-        # xlim = np.log(ax.get_xlim()[1])
     
-    plt.xlabel('Elapsed time [min]', size=16)
-    plt.ylabel('Drawdown [m]', size=16)
-    plt.title(f'{title}', size=24)
+    y0 = m * np.log(t0) + c
+    y1 = m * np.log(t0*10) + c
     
+    ylim = ax.get_ylim()[1]
+    xlim = np.log(ax.get_xlim()[1])
+    
+    plt.axvline(x=t0, ymax=y0/ylim, color='blue', linestyle='--')
+    plt.axvline(x=t0*10, ymax=y1/ylim, color='blue', linestyle='--')
+    
+    plt.axhline(y=y0, xmax=np.log(t0)/xlim, color='blue', linestyle='--')
+    plt.axhline(y=y1, xmax=np.log(t0*10)/xlim, color='blue', linestyle='--')
+    
+    ds = y1 - y0
+    
+    plt.xlabel('Elapsed time [min]', size=14)
+    plt.ylabel('Drawdown [m]', size=14)
+    plt.title(f'{title}', size=22)
+
+    # text = f'{m:.3f}\u00b7t + {c:.3f}'
+    # plt.text(x=1.2, y=.2, s=text, horizontalalignment='left', size='large')
+
     plt.tight_layout()
     plt.grid()
     
+    d = {
+        'ds': ds,
+        'y_fit_m': m,
+        'y_fit_c': c
+        }
+    
     # plt.savefig(f'{bh_name}_Constant.png')
     # plt.show()
-    return ax
+    return ax, d
 
 def normal_drawdown_plotly(t:pd.Series, ds:pd.Series, reverse_y=True, title='Your title here', log_x=False, guess=15, t0=10):
     """
